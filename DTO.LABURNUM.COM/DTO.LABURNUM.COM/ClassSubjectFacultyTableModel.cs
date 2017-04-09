3c61ab7efad4bea34974696f20d183481a4dfaa4
Nishant Chauhan <nishant.chau@gmail.com>
2017-04-09 22:15:38 +0530

    DTO Changes

--- DTO.LABURNUM.COM/DTO.LABURNUM.COM/ClassSubjectFacultyTableModel.cs
+++ DTO.LABURNUM.COM/DTO.LABURNUM.COM/ClassSubjectFacultyTableModel.cs
@@ -8,6 +8,10 @@ namespace DTO.LABURNUM.COM
 {
     public class ClassSubjectFacultyTableModel
     {
+        public ClassSubjectFacultyTableModel()
+        {
+            this.ApiClientModel = new ApiClientModel();
+        }
         public long ClassSubjectFacultyTableId { get; set; }
         public long ClassId { get; set; }
         public long SubjectId { get; set; }
@@ -16,5 +20,11 @@ namespace DTO.LABURNUM.COM
         public bool IsActive { get; set; }
         public System.DateTime CreatedOn { get; set; }
         public Nullable<System.DateTime> LastUpdated { get; set; }
+
+        public ApiClientModel ApiClientModel { get; set; }
+        public List<DTO.LABURNUM.COM.ClassModel> Classes { get; set; }
+        public List<DTO.LABURNUM.COM.SectionModel> Sections { get; set; }
+        public List<DTO.LABURNUM.COM.SubjectModel> Subjects { get; set; }
+        public List<DTO.LABURNUM.COM.FacultyModel> Faculties { get; set; }
     }
 }
