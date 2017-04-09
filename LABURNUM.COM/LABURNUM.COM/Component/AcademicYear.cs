1bbafbda0e827c72ccee31445a629f81b172d2d6
Nishant Chauhan <nishant.chau@gmail.com>
2017-04-09 22:16:10 +0530

    Project Changes

--- LABURNUM.COM/LABURNUM.COM/Component/AcademicYear.cs
+++ LABURNUM.COM/LABURNUM.COM/Component/AcademicYear.cs
@@ -3,62 +3,61 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Web;
 
 namespace LABURNUM.COM.Component
 {
-    public class AdmissionType
+    public class AcademicYear
     {
-        public List<DTO.LABURNUM.COM.AdmissionTypeModel> GetActiveAdmissionTypes()
+        public List<DTO.LABURNUM.COM.AcademicYearTableModel> GetActiveAcademicYears()
         {
             try
             {
-                DTO.LABURNUM.COM.ClassModel model = new DTO.LABURNUM.COM.ClassModel();
+                DTO.LABURNUM.COM.AcademicYearTableModel model = new DTO.LABURNUM.COM.AcademicYearTableModel();
                 model.ApiClientModel = new LABURNUM.COM.Component.Common().GetApiClientModel();
                 HttpClient client = new LABURNUM.COM.Component.Common().GetHTTPClient("application/json");
-                HttpResponseMessage response = client.PostAsJsonAsync("AdmissionType/SearchActiveAdmissionTypes", model).Result;
+                HttpResponseMessage response = client.PostAsJsonAsync("AcademicYear/SearchActiveAcademicYears", model).Result;
                 if (response.IsSuccessStatusCode)
                 {
                     var data = response.Content.ReadAsStringAsync().Result;
-                    return JsonConvert.DeserializeObject<List<DTO.LABURNUM.COM.AdmissionTypeModel>>(data);
+                    return JsonConvert.DeserializeObject<List<DTO.LABURNUM.COM.AcademicYearTableModel>>(data);
                 }
                 else { return null; }
             }
             catch (Exception)
             {
-                throw new Exception("Error While Getting AdmissionType List");
+                throw new Exception("Error While Getting Academic Year Table Model List");
             }
         }
 
-        public List<DTO.LABURNUM.COM.AdmissionTypeModel> GetAllAdmissionTypes()
+        public List<DTO.LABURNUM.COM.AcademicYearTableModel> GetAllAdmissionTypes()
         {
             try
             {
-                DTO.LABURNUM.COM.AdmissionTypeModel model = new DTO.LABURNUM.COM.AdmissionTypeModel();
+                DTO.LABURNUM.COM.AcademicYearTableModel model = new DTO.LABURNUM.COM.AcademicYearTableModel();
                 model.ApiClientModel = new LABURNUM.COM.Component.Common().GetApiClientModel();
                 HttpClient client = new LABURNUM.COM.Component.Common().GetHTTPClient("application/json");
-                HttpResponseMessage response = client.PostAsJsonAsync("AdmissionType/SearchAllAdmissionTypes", model).Result;
+                HttpResponseMessage response = client.PostAsJsonAsync("AcademicYear/SearchAllAcademicYears", model).Result;
                 if (response.IsSuccessStatusCode)
                 {
                     var data = response.Content.ReadAsStringAsync().Result;
-                    return JsonConvert.DeserializeObject<List<DTO.LABURNUM.COM.AdmissionTypeModel>>(data);
+                    return JsonConvert.DeserializeObject<List<DTO.LABURNUM.COM.AcademicYearTableModel>>(data);
                 }
                 else { return null; }
             }
             catch (Exception)
             {
-                throw new Exception("Error While Getting AdmissionType List");
+                throw new Exception("Error While Getting Academic Year Table Model List");
             }
         }
 
-        public void UpdateAdmissionTypeStatus(DTO.LABURNUM.COM.AdmissionTypeModel model)
+        public void UpdateClasseStatus(DTO.LABURNUM.COM.AcademicYearTableModel model)
         {
             try
             {
                 model.ApiClientModel = new LABURNUM.COM.Component.Common().GetApiClientModel();
                 HttpClient client = new LABURNUM.COM.Component.Common().GetHTTPClient("application/json");
-                HttpResponseMessage response = client.PostAsJsonAsync("AdmissionType/UpdateStatus", model).Result;
+                HttpResponseMessage response = client.PostAsJsonAsync("AcademicYear/UpdateStatus", model).Result;
                 if (response.IsSuccessStatusCode)
                 {
 
@@ -67,21 +66,21 @@ namespace LABURNUM.COM.Component
             }
             catch (Exception)
             {
-                throw new Exception("Error While Updating AdmissionType Status");
+                throw new Exception("Error While Updating Academic Year Status");
             }
         }
 
-        public List<DTO.LABURNUM.COM.AdmissionTypeModel> GetAdmissionTypeByAdvanceSearch(DTO.LABURNUM.COM.AdmissionTypeModel model)
+        public List<DTO.LABURNUM.COM.AcademicYearTableModel> GetAcademicYearByAdvanceSearch(DTO.LABURNUM.COM.AcademicYearTableModel model)
         {
             try
             {
                 model.ApiClientModel = new LABURNUM.COM.Component.Common().GetApiClientModel();
                 HttpClient client = new LABURNUM.COM.Component.Common().GetHTTPClient("application/json");
-                HttpResponseMessage response = client.PostAsJsonAsync("AdmissionType/SearchAdmissionTypeByAdvanceSearch", model).Result;
+                HttpResponseMessage response = client.PostAsJsonAsync("AcademicYear/SearchAcademicYearByAdvanceSearch", model).Result;
                 if (response.IsSuccessStatusCode)
                 {
                     var data = response.Content.ReadAsStringAsync().Result;
-                    return JsonConvert.DeserializeObject<List<DTO.LABURNUM.COM.AdmissionTypeModel>>(data);
+                    return JsonConvert.DeserializeObject<List<DTO.LABURNUM.COM.AcademicYearTableModel>>(data);
                 }
                 else { return null; }
             }
@@ -91,12 +90,12 @@ namespace LABURNUM.COM.Component
             }
         }
 
-        public DTO.LABURNUM.COM.AdmissionTypeModel GetAdmissionTypeByAdmissionTypeId(long id)
+        public DTO.LABURNUM.COM.AcademicYearTableModel GetAcademicYearByAcademicYearId(long id)
         {
             try
             {
-                DTO.LABURNUM.COM.AdmissionTypeModel model = new DTO.LABURNUM.COM.AdmissionTypeModel() { AdmissionTypeId = id };
-                List<DTO.LABURNUM.COM.AdmissionTypeModel> dbAdmissionTypes = GetAdmissionTypeByAdvanceSearch(model);
+                DTO.LABURNUM.COM.AcademicYearTableModel model = new DTO.LABURNUM.COM.AcademicYearTableModel() { AcademicYearTableId = id };
+                List<DTO.LABURNUM.COM.AcademicYearTableModel> dbAdmissionTypes = GetAcademicYearByAdvanceSearch(model);
                 if (dbAdmissionTypes.Count == 0) { throw new Exception(LABURNUM.COM.Component.Constants.ERRORMESSAGES.NO_RECORD_FOUND); }
                 if (dbAdmissionTypes.Count > 1) { throw new Exception(LABURNUM.COM.Component.Constants.ERRORMESSAGES.MORE_THAN_ONE_RECORDFOUND); }
                 return dbAdmissionTypes[0];
--- LABURNUM.COM/LABURNUM.COM/Component/ClassSubjectFacultyTable.cs
+++ LABURNUM.COM/LABURNUM.COM/Component/ClassSubjectFacultyTable.cs
@@ -3,62 +3,61 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Web;
 
 namespace LABURNUM.COM.Component
 {
-    public class AdmissionType
+    public class ClassSubjectFacultyTable
     {
-        public List<DTO.LABURNUM.COM.AdmissionTypeModel> GetActiveAdmissionTypes()
+        public List<DTO.LABURNUM.COM.ClassSubjectFacultyTableModel> GetActiveClassSubjectFaculties()
         {
             try
             {
-                DTO.LABURNUM.COM.ClassModel model = new DTO.LABURNUM.COM.ClassModel();
+                DTO.LABURNUM.COM.ClassSubjectFacultyTableModel model = new DTO.LABURNUM.COM.ClassSubjectFacultyTableModel();
                 model.ApiClientModel = new LABURNUM.COM.Component.Common().GetApiClientModel();
                 HttpClient client = new LABURNUM.COM.Component.Common().GetHTTPClient("application/json");
-                HttpResponseMessage response = client.PostAsJsonAsync("AdmissionType/SearchActiveAdmissionTypes", model).Result;
+                HttpResponseMessage response = client.PostAsJsonAsync("ClassSubjectFacultyTable/SearchActiveClassSubjectFaculties", model).Result;
                 if (response.IsSuccessStatusCode)
                 {
                     var data = response.Content.ReadAsStringAsync().Result;
-                    return JsonConvert.DeserializeObject<List<DTO.LABURNUM.COM.AdmissionTypeModel>>(data);
+                    return JsonConvert.DeserializeObject<List<DTO.LABURNUM.COM.ClassSubjectFacultyTableModel>>(data);
                 }
                 else { return null; }
             }
             catch (Exception)
             {
-                throw new Exception("Error While Getting AdmissionType List");
+                throw new Exception("Error While Getting Class Subject Faculty Table Model List");
             }
         }
 
-        public List<DTO.LABURNUM.COM.AdmissionTypeModel> GetAllAdmissionTypes()
+        public List<DTO.LABURNUM.COM.ClassSubjectFacultyTableModel> GetAllClassSubjectFaculties()
         {
             try
             {
-                DTO.LABURNUM.COM.AdmissionTypeModel model = new DTO.LABURNUM.COM.AdmissionTypeModel();
+                DTO.LABURNUM.COM.ClassSubjectFacultyTableModel model = new DTO.LABURNUM.COM.ClassSubjectFacultyTableModel();
                 model.ApiClientModel = new LABURNUM.COM.Component.Common().GetApiClientModel();
                 HttpClient client = new LABURNUM.COM.Component.Common().GetHTTPClient("application/json");
-                HttpResponseMessage response = client.PostAsJsonAsync("AdmissionType/SearchAllAdmissionTypes", model).Result;
+                HttpResponseMessage response = client.PostAsJsonAsync("ClassSubjectFacultyTable/SearchAllClassSubjectFaculties", model).Result;
                 if (response.IsSuccessStatusCode)
                 {
                     var data = response.Content.ReadAsStringAsync().Result;
-                    return JsonConvert.DeserializeObject<List<DTO.LABURNUM.COM.AdmissionTypeModel>>(data);
+                    return JsonConvert.DeserializeObject<List<DTO.LABURNUM.COM.ClassSubjectFacultyTableModel>>(data);
                 }
                 else { return null; }
             }
             catch (Exception)
             {
-                throw new Exception("Error While Getting AdmissionType List");
+                throw new Exception("Error While Getting Class Subject Faculty Table Model List");
             }
         }
 
-        public void UpdateAdmissionTypeStatus(DTO.LABURNUM.COM.AdmissionTypeModel model)
+        public void UpdateClassSubjectFacultyStatus(DTO.LABURNUM.COM.ClassSubjectFacultyTableModel model)
         {
             try
             {
                 model.ApiClientModel = new LABURNUM.COM.Component.Common().GetApiClientModel();
                 HttpClient client = new LABURNUM.COM.Component.Common().GetHTTPClient("application/json");
-                HttpResponseMessage response = client.PostAsJsonAsync("AdmissionType/UpdateStatus", model).Result;
+                HttpResponseMessage response = client.PostAsJsonAsync("ClassSubjectFacultyTable/UpdateStatus", model).Result;
                 if (response.IsSuccessStatusCode)
                 {
 
@@ -67,36 +66,36 @@ namespace LABURNUM.COM.Component
             }
             catch (Exception)
             {
-                throw new Exception("Error While Updating AdmissionType Status");
+                throw new Exception("Error While Updating Class Subject Faculty Table Status");
             }
         }
 
-        public List<DTO.LABURNUM.COM.AdmissionTypeModel> GetAdmissionTypeByAdvanceSearch(DTO.LABURNUM.COM.AdmissionTypeModel model)
+        public List<DTO.LABURNUM.COM.ClassSubjectFacultyTableModel> GetClassSubjectFacultyByAdvanceSearch(DTO.LABURNUM.COM.ClassSubjectFacultyTableModel model)
         {
             try
             {
                 model.ApiClientModel = new LABURNUM.COM.Component.Common().GetApiClientModel();
                 HttpClient client = new LABURNUM.COM.Component.Common().GetHTTPClient("application/json");
-                HttpResponseMessage response = client.PostAsJsonAsync("AdmissionType/SearchAdmissionTypeByAdvanceSearch", model).Result;
+                HttpResponseMessage response = client.PostAsJsonAsync("ClassSubjectFacultyTable/SearchClassSubjectFacultyByAdvanceSearch", model).Result;
                 if (response.IsSuccessStatusCode)
                 {
                     var data = response.Content.ReadAsStringAsync().Result;
-                    return JsonConvert.DeserializeObject<List<DTO.LABURNUM.COM.AdmissionTypeModel>>(data);
+                    return JsonConvert.DeserializeObject<List<DTO.LABURNUM.COM.ClassSubjectFacultyTableModel>>(data);
                 }
                 else { return null; }
             }
             catch (Exception)
             {
-                throw new Exception("Error While Getting Admission Type Model List");
+                throw new Exception("Error While Getting Class Subject Faculty Table Model List");
             }
         }
 
-        public DTO.LABURNUM.COM.AdmissionTypeModel GetAdmissionTypeByAdmissionTypeId(long id)
+        public DTO.LABURNUM.COM.ClassSubjectFacultyTableModel GetClassSubjectFacultyById(long id)
         {
             try
             {
-                DTO.LABURNUM.COM.AdmissionTypeModel model = new DTO.LABURNUM.COM.AdmissionTypeModel() { AdmissionTypeId = id };
-                List<DTO.LABURNUM.COM.AdmissionTypeModel> dbAdmissionTypes = GetAdmissionTypeByAdvanceSearch(model);
+                DTO.LABURNUM.COM.ClassSubjectFacultyTableModel model = new DTO.LABURNUM.COM.ClassSubjectFacultyTableModel() { ClassSubjectFacultyTableId = id };
+                List<DTO.LABURNUM.COM.ClassSubjectFacultyTableModel> dbAdmissionTypes = GetClassSubjectFacultyByAdvanceSearch(model);
                 if (dbAdmissionTypes.Count == 0) { throw new Exception(LABURNUM.COM.Component.Constants.ERRORMESSAGES.NO_RECORD_FOUND); }
                 if (dbAdmissionTypes.Count > 1) { throw new Exception(LABURNUM.COM.Component.Constants.ERRORMESSAGES.MORE_THAN_ONE_RECORDFOUND); }
                 return dbAdmissionTypes[0];
