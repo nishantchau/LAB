fa8cd7fcdd12e5b7ccc148a10e628064cbd27406
Nishant Chauhan <nishant.chau@gmail.com>
2017-04-11 22:31:09 +0530

    Student Fee Reporting

---
+++ API.LABURNUM.COM/API.LABURNUM.COM/Component/FeeReportingResultModelHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API.LABURNUM.COM.Component
+{
+    public class FeeReportingResultModelHelper
+    {
+        public DTO.LABURNUM.COM.FeeReportingResultModel GetFeeReportingResultModel(API.LABURNUM.COM.StudentFee dbStudentFee)
+        {
+            DTO.LABURNUM.COM.FeeReportingResultModel model = new DTO.LABURNUM.COM.FeeReportingResultModel()
+            {
+                AcademicYearId = dbStudentFee.AcademicYearId,
+                AcademicYear = dbStudentFee.AcademicYearTable.AcademicYear,
+                AdmissionFee = dbStudentFee.AdmissionFee,
+                AnnualCharges = dbStudentFee.AnnualCharges,
+                DevelopementFee = dbStudentFee.DevelopementFee,
+                ExamFee = dbStudentFee.ExamFee,
+                SportsFee = dbStudentFee.SportsFee,
+                SecurityFee = dbStudentFee.SecurityFee,
+                DiscountAmount = dbStudentFee.DiscountAmount,
+                ClassId = dbStudentFee.ClassId,
+                ClassName = dbStudentFee.Class.ClassName,
+                StudentId = dbStudentFee.StudentId,
+                StudentName = dbStudentFee.Student.FirstName + " " + dbStudentFee.Student.MiddleName + " " + dbStudentFee.Student.LastName,
+                SectionId = dbStudentFee.SectionId,
+                SectionName = dbStudentFee.Section.SectionName
+            };
+            return model;
+        }
+    }
+}
---
+++ API.LABURNUM.COM/API.LABURNUM.COM/Controllers/FeeReportingController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using API.LABURNUM.COM.Component;
+
+namespace API.LABURNUM.COM.Controllers
+{
+    public class FeeReportingController : ApiController
+    {
+        public DTO.LABURNUM.COM.FeeReportingResultModel SearchStudentReporting(DTO.LABURNUM.COM.FeeReportingModel model)
+        {
+            if (new FrontEndApi.ApiClientApi().IsClientValid(model.ApiClientModel.UserName, model.ApiClientModel.Password))
+            {
+                DTO.LABURNUM.COM.FeeReportingResultModel report = new DTO.LABURNUM.COM.FeeReportingResultModel();
+                List<API.LABURNUM.COM.StudentFee> dbStudentFee = new FrontEndApi.StudentFeeApi().GetStudentFeeByAdvanceSearch(new DTO.LABURNUM.COM.StudentFeeModel() { AcademicYearId = model.AcademicYearId, StudentId = model.StudentId });
+                if (dbStudentFee.Count > 0)
+                {
+                    report = new Component.FeeReportingResultModelHelper().GetFeeReportingResultModel(dbStudentFee[0]);
+                }
+                report.MonthlyFeeDetails = new StudentFeeDetailHelper(new FrontEndApi.StudentFeeDetailApi().GetStudentFeeDetailByAdvanceSearch(new DTO.LABURNUM.COM.StudentFeeDetailModel() { AcademicYearId = model.AcademicYearId, StudentId = model.StudentId })).Map();
+                return report;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
