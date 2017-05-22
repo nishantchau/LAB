a5bbbd57a922ea7e34dcd9a7e1f18c9e74b983be
Nishant Chauhan <nishant.chau@gmail.com>
2017-05-23 00:27:38 +0530

    DTo Project Commit

---
+++ DTO.LABURNUM.COM/DTO.LABURNUM.COM/AlbumDetailModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO.LABURNUM.COM
+{
+    public class AlbumDetailModel
+    {
+        public AlbumDetailModel()
+        {
+            this.ApiClientModel = new ApiClientModel();
+        }
+
+        public long AlbumDetailId { get; set; }
+        public long AlbumId { get; set; }
+        public string Attachment { get; set; }
+        public bool IsActive { get; set; }
+        public System.DateTime CreatedOn { get; set; }
+        public Nullable<System.DateTime> LastUpdated { get; set; }
+
+        public ApiClientModel ApiClientModel { get; set; }
+
+        public string AlbumName { get; set; }
+    }
+}
---
+++ DTO.LABURNUM.COM/DTO.LABURNUM.COM/AlbumModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO.LABURNUM.COM
+{
+    public class AlbumModel
+    {
+        public AlbumModel()
+        {
+            this.ApiClientModel = new ApiClientModel();
+            this.AlbumDetails = new List<AlbumDetailModel>();
+        }
+
+        public long AlbumId { get; set; }
+        public string AlbumName { get; set; }
+        public string AlbumCoverImage { get; set; }
+        public long AddedById { get; set; }
+        public Nullable<long> UpdatedById { get; set; }
+        public bool IsActive { get; set; }
+        public System.DateTime CreatedOn { get; set; }
+        public Nullable<System.DateTime> LastUpdated { get; set; }
+
+        public DTO.LABURNUM.COM.ApiClientModel ApiClientModel { get; set; }
+        public List<DTO.LABURNUM.COM.AlbumDetailModel> AlbumDetails { get; set; }
+
+        public string AddedByName { get; set; }
+        public string UpdatedByName { get; set; }
+    }
+}
